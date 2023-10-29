5c73ade2d5284637556e4f15943ac09867aafd10
Devesh91 <1991devesh@gmail.com>
2023-10-29 22:19:36 +0530

    added decorator code

---
+++ Usecases/decorator.py
@@ -0,0 +1,21 @@
+"""This file contains example of decorator with arguments"""
+
+def make_pretty(*args, **kwargs):
+
+    def inner(func):
+
+        print("my func just got decorated")
+        print("api name {}".format(kwargs['x']))
+        print(*args)
+
+        return func
+
+    return inner
+
+
+@make_pretty((1,2,3),x="my_func_api")
+def my_func():
+    return "Hello World"
+
+
+print(my_func())
